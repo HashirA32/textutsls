f20e8422cc54b5c4e17f758596abeae86b59a431
HashirA32 <hashirofficiala32@gmail.com>
2024-05-25 12:29:07 +0500

    Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './comps/Navbar';
+import PropTypes from 'prop-types';
+import Footer from './comps/Footer';
+import Content from './comps/Content';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-            Hashir
-        </a>
-      </header>
-    </div>
+   <> 
+      <div className="fractions">
+      <Navbar title = "Hashir" about = "about"/>
+      < Content/>
+      <Footer/>
+      </div>
+   </>
   );
 }
 
 export default App;
+Navbar.PropTypes = {title : PropTypes.string,
+                    about : PropTypes.string,
+}
---
+++ src/comps/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer(props) {
+  return (
+    <>
+    <div className="card text-center">
+  <div className="card-header">
+    About
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Special title treatment</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="/" className="btn btn-primary">Go to Top</a>
+  </div>
+  
+</div>
+    </>
+   );
+  }
+  
