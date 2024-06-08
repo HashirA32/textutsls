97c30d3ac58defb770236ef27a7c05ef91abd82a
HashirA32 <hashirofficiala32@gmail.com>
2024-06-08 22:41:50 +0500

    Change Color

--- src/comps/Content.js
+++ src/comps/Content.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 export default  function Content(props){
+
+  
+  let changeColor =  () =>  {
+    if(myStyle.color === 'rgba(16, 21, 27, 0.918)'){
+        newStyle ( {
+          color: 'white',
+          backgroundColor: 'rgba(16, 21, 27, 0.918)'
+        }) 
+    } else {
+      newStyle ( {
+        color: 'rgba(16, 21, 27, 0.918)',
+        backgroundColor: 'white',
+        
+      })
+    }
+}
+let [myStyle, newStyle] = useState({
+  color: 'rgba(16, 21, 27, 0.918)',
+  backgroundColor: 'white'
+} );
+
+
   return(
     <>
-  <div className="Boxs">  
+     <button onClick={changeColor} className='btn btn-primary'>Change Color</button>
+  <div className="Boxs" style={myStyle}>  
+ 
 
     <div className="cards">
+
+
+    
 
       <div className="card" >
         <img src="tv.png" className="card-img-top" alt="..."/>
